71ae6cd3d0d209946975d770d3fa7a527df2d4a1
irrotation <jedatermy87@gmail.com>
2015-04-10 20:00:11 +0300

    DefaultDeefParser and PipedRequest separated as own modules. PiperRequest ready

---
+++ routes/PipedRequest.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var request = require('request');
+
+module.exports = function PipedRequest(uri, pipeTarget) {
+
+   var self = this;
+   this.targetUri = uri;
+   this.pipeTarget = pipeTarget;
+
+   this.pipe = function() {
+      var stream = request.get(self.targetUri);
+      stream.on('error', handleError);
+      stream.on('response', function onResponse(res) {
+         res.pipe(self.pipeTarget);
+      });
+   }
+
+   function handleError(err) {
+      console.log(err, err.stack);
+      return process.exit(1);
+   }
+}
--- routes/RssParser.js
+++ routes/RssParser.js
@@ -1,20 +1,26 @@
 'use strict';
 
-var FeedParser = require('feedparser');
-var request = require('request');
-var fs = require('fs');
-var path = require('path');
-var Promise = require('bluebird');
+var PipedRequest = require('./PipedRequest');
+var DefaultFeedParser = require('./DefaultFeedParser');
 
 var lastResult = [];
 
 module.exports = function RssParser () {
 
+   var self = this;
    this.makeRequest = function (rssAddress, callback) {
 
       callback = (typeof callback === 'function') 
          ? callback 
          : function() {};
+
+      var feedparser = new DefaultFeedParser(self.handleRssItem, handleResults)
+
+      function handleResults(results) {
+         
+      }
+      var request = new PipedRequest(rssAddress, feedparser);
+      request.pipe();
       
       requestRss(rssAddress, feedparser);
    }
@@ -25,68 +31,6 @@ module.exports = function RssParser () {
          : function() {};
 
       callback(null, item); 
-   }
-
-   function DefaultFeedParser(rssItemHandler, callback) {
-
-      var self = this;
-      var results = [];
-      var feedparser = new FeedParser();
-
-      var rssItemHandler = (typeof rssItemHandler === 'function') 
-         ? rssItemHandler 
-         : defaultItemHandler
-
-      function defaultItemHandler (item, callback) {
-         console.log("item handled");
-         callback(null, item);
-      }
-
-      var callback = (typeof callback === 'function') 
-         ? callback 
-         : function noCallback() {};
-      
-      feedparser.on('error', function onError(err) {
-         console.log(err);
-      });
-
-      feedparser.on('readable', function onReadable() {
-         
-         var stream = this;
-         var item;
-         while (item = stream.read()) {
-
-            rssItemHandler(item, afterHandling);
-
-            function afterHandling(item) {
-               console.log("item pushed");
-               self.results.push(item);
-            }
-         }
-      });
-
-      feedparser.on('end', function onEnd() {
-         self.callback(null, results);
-      });
-   }
-  
-   function PipedRequest(uri, pipeTarget) {
-
-      this.targetUri = uri;
-      this.pipeTarget = pipeTarget;
-
-      this.pipe = function() {
-         var stream = request.get(uri);
-         stream.on('error', handleError);
-         stream.on('response', function onResponse(res) {
-            res.pipe(pipeTarget);
-         });
-      }
-
-      function handleError(err) {
-         console.log(err, err.stack);
-         return process.exit(1);
-      }
    }
 }
 
