de18b6c96e1696992669cc9a0702b7f24954b257
irrotation <jedatermy87@gmail.com>
2015-04-29 19:39:13 +0300

    events table

---
+++ models/events.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = function createEvent (sequelize, Datatypes) {
+
+   var Event = sequelize.define('Event', {
+
+      id: {
+         type: Datatypes.BIGINT(20),
+         primaryKey: true,
+         autoIncrement: true
+      },
+
+      title: {
+         type: Datatypes.TEXT,
+         allowNull: false
+      },
+
+      startDate: {
+         type: Datatypes.DATE,
+         allowNull: false
+      },
+
+      endDate: {
+         type: Datatypes.DATE,
+         allowNull: false
+      },
+
+      location: {
+         type: Datatypes.TEXT
+      },
+
+      meetupLocation: {
+         type: Datatypes.TEXT
+      },
+
+      description: {
+         type: Datatypes.TEXT
+      },
+
+      eventWebpage: {
+         type: Datatypes.TEXT,
+         allowNull: false
+      }
+
+   }, {
+      tableName: 'events',
+
+      hooks: {
+         beforeCreate: function(event, options, callback) {
+            if(!event.meetupLocation) {
+               event.meetupLocation = 'Tarkempi ajankohta selviää lähempänä tapahtumaa';
+            }
+
+            callback(null, event)
+         }
+      }
+   });
+
+   return Event;
+}
